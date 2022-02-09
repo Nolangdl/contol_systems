483e84246effedb050dd69818f751f03f0bf78b5
nlucero22 <59714989+Nolangdl@users.noreply.github.com>
2022-02-09 10:21:28 +0700

    Create cp_task_4.py

---
+++ cp_task_4.py
@@ -0,0 +1,12 @@
+"""CircuitPython Essentials Analog In example"""
+import time
+import board
+from analogio import AnalogIn
+
+# Connect an analog sensor to the A1 port of the board you are using. This means a 3V, GND, and signal pin must all be connected.
+light = AnalogIn(board.A1)
+LED = AnalogOut(board.A0)
+# This function turns the analog reading of the sensor (a number from 0 - 65536) to a voltage.
+
+while True:
+    LED = light - 100 * -1 
